8d09fab401486379a8b0a3310e40eeb1aad617fb
Exo Des <9076458+eXodes@users.noreply.github.com>
2024-12-22 05:08:32 +0800

    fix(web): layout and index page

--- apps/web/tests/page.test.ts
+++ apps/web/tests/page.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from "@playwright/test";
 
-test("index page has expected h1", async ({ page }) => {
+test("index page has expected title", async ({ page }) => {
   await page.goto("/");
-  await expect(page.getByRole("heading", { name: "Hello" })).toBeVisible();
+
+  expect(await page.title()).toBe("Exo Des");
 });
