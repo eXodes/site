741c761960c4c8a089d245f881f8227ad308b918
Exo Des <9076458+eXodes@users.noreply.github.com>
2024-12-22 05:05:38 +0800

    chore(ui): prettier format

--- packages/ui/src/common/link/link.svelte
+++ packages/ui/src/common/link/link.svelte
@@ -13,7 +13,7 @@
 
 <a
   {...props}
-  class="font-medium bg-[length:100%_200%] transition-all duration-500 bg-gradient-to-b from-transparent from-50% to-yellow to-50% border-b-4 border-yellow px-0.5 -mx-0.5 hover:bg-[0_100%] hover:border-b-2"
+  class="-mx-0.5 border-b-4 border-yellow bg-gradient-to-b from-transparent from-50% to-yellow to-50% bg-[length:100%_200%] px-0.5 font-medium transition-all duration-500 hover:border-b-2 hover:bg-[0_100%]"
   href={href}
   target={external ? "_blank" : undefined}
 >
