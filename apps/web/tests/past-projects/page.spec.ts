74dd5f03f3dce3a0ef17a17d7e454909fd997729
Exo Des <9076458+eXodes@users.noreply.github.com>
2024-12-22 14:35:44 +0800

    fix(web): past projects detail

--- apps/web/tests/past-projects/page.spec.ts
+++ apps/web/tests/past-projects/page.spec.ts
@@ -1,7 +1,19 @@
 import { expect, test } from "@playwright/test";
 
-test("Projects page has expected title", async ({ page }) => {
-  await page.goto("/past-projects");
+test.describe("Projects (/past-projects)", () => {
+  test("has expected title", async ({ page }) => {
+    await page.goto("/past-projects");
 
-  expect(await page.title()).toBe("Projects | Exo Des");
+    expect(await page.title()).toBe("Projects | Exo Des");
+  });
+
+  test("should have a list of projects", async ({ page }) => {
+    await page.goto("/past-projects");
+
+    const heading = await page.$("h1");
+    expect(await heading?.textContent()).toBe("Past Projects");
+
+    const projects = await page.$$("li");
+    expect(projects.length).toBe(15);
+  });
 });
